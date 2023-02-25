dc1feb02967b2dcb0b0c9c22309420d537c7eb9a
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-02-25 14:49:29 +0530

    [ADDED]Bhavesh:The Program to find the area of shapes using Class.

---
+++ OOPS36.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+class Shape {
+ protected:
+  float area;
+};
+class Rectangle : public Shape {
+ private:
+  float length, breadth;
+ public:
+  void get() {
+    cout << "\nEnter Rectangle's Length :";
+    cin >> length;
+    cout << "Enter Rectangle's Breadth :";
+    cin >> breadth;
+  }
+  double Area() {
+    area = length * breadth;
+    return area;
+  }
+};
+class Circle : public Shape {
+ private:
+  float radius;
+ public:
+  void get() {
+    cout << "Enter Circle's Radius :";
+    cin >> radius;
+  }
+  double Area() {
+    area = (22 * (radius * radius)) / 7;
+    return area;
+  }
+};
+class Square : public Shape {
+ private:
+  float side;
+ public:
+  void get() {
+    cout << "\nEnter Square's Side : ";
+    cin >> side;
+  }
+  double Area() {
+    area = side * side;
+    return area;
+  }
+};
+
+int main() 
+{
+  Circle C;
+  float circle;
+  C.get();
+  circle = C.Area();
+  cout << "Area of Circle : " << circle << endl;
+  Rectangle R;
+  float rectangle;
+  R.get();
+  rectangle = R.Area();
+  cout << "Area of Rectangle : " << rectangle << endl;
+  Square S;
+  float square;
+  S.get();
+  square = S.Area();
+  cout << "Area of Square : " << square << endl;
+  return 0;
+}
