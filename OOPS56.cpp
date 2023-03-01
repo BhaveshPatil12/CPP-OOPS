61cf39f46fd4a0f7a5ab70bba1f3b6e9631a2d7e
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-03-01 16:29:53 +0530

    [ADDED]Bhavesh:The Program to sort the string of characters in ascending order using Class.

---
+++ OOPS56.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class String {
+ private:
+  char str[30];
+ public:
+  void getString() {
+    cout << "Enter String : ";
+    cin.getline(str, 30);
+  }
+  string sortAscending() {
+    int index_1, index_2, temp, length;
+    for (length = 0; str[length]; length++);
+    length--;
+    for (index_1 = 0; str[index_1]; index_1++) {
+      for (index_2 = 0; index_2 < length - index_1; index_2++) {
+        if (str[index_2] > str[index_2 + 1]) {
+          temp = str[index_2];
+          str[index_2] = str[index_2 + 1];
+          str[index_2 + 1] = temp;
+        }
+      }
+    }
+    return str;
+  }
+};
+
+int main() 
+{
+  String S;
+  string str;
+  S.getString();
+  str = S.sortAscending();
+  cout << "Sorted String is " << str;
+  return 0;
+}
