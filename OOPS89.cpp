4f226ed61d9152d922de240ff4d1f0cb0671394a
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-03-07 21:30:12 +0530

    [ADDED]Bhavesh:The Program to find the sum of two arrays one in reverse using Class.

---
+++ OOPS89.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class Array {
+ private:
+  int arr1[10];
+  int arr2[10];
+ public:
+  void getArray() {
+    cout << "Input 1st Array :" << endl;
+    for (int index = 0; index < 10; index++) {
+      cout << "[" << index << "]: ";
+      cin >> arr1[index];
+    }
+
+    cout << "\nInput 2nd Array :" << endl;
+
+    for (int index = 0; index < 10; index++) {
+      cout << "[" << index << "]: ";
+      cin >> arr2[index];
+    }
+  }
+  void addArray() {
+    int sum[10], index;
+    for (index = 0; index < 10; index++) {
+      sum[index] = arr1[index] + arr2[9 - index];
+    }
+    cout << "\nReversely added array :" << endl;
+    for (index = 0; index < 10; index++) {
+      cout << sum[index] << " ";
+    }
+  }
+};
+
+int main() 
+{
+  Array A;
+  A.getArray();
+  A.addArray();
+  return 0;
+}
