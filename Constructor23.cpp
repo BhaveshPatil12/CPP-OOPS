4025b64be0077d0060c2f996b8245d2987215ecb
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-04-03 10:01:19 +0530

    [ADDED]Bhavesh:The Program of Dynamic constructor.

---
+++ Constructor23.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class Mania
+{
+  int num1;
+  int num2;
+  int *ptr;
+public:
+  Mania()
+  {
+    num1 = 0;
+    num2 = 0;
+    ptr = new int;
+  }
+  Mania(int x, int y, int z)
+  {
+    num1 = x;
+    num2 = y;
+    
+    ptr = new int;
+    *ptr = z;
+  }
+ 
+ void display()
+ {
+   cout << num1 << " " << num2 << " " << *ptr;
+ }
+};
+
+int main()
+{
+  Mania obj1;
+  Mania obj2(3, 5, 11);
+  obj1.display();
+  cout << endl;
+  obj2.display();
+}
