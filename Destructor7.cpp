18fbdea2fc631b5b204aa09cf6bbb62acd27c48e
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-04-08 10:13:42 +0530

    [ADDED]Bhavesh:The Program for Upcasting without virtual destructor.

---
+++ Destructor7.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class base {
+   public:
+    ~base() {
+        cout << "Destructor base." << endl;
+    }
+};
+class derived : publicbase {
+    public:
+    ~derived() {
+        cout << "Destructor derived." << endl;
+    }
+}
+
+int main() {
+    base* b = new derived;
+    delete b;
+    return 0;
+}
