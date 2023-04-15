b780584bea9014b62c600ef14aaf99c7a2295647
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-04-15 11:51:36 +0530

    [ADDED]Bhavesh:The Program of Virtual destructor is to destruct the resources in a proper order, when you delete a base class pointer pointing to derived class object.

---
+++ Destructor28.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Base
+{
+public:
+    Base(){
+        cout << "Base Constructor Called\n";
+    }
+    ~Base(){
+        cout << "Base Destructor called\n";
+    }
+};
+
+class Derived1: public Base
+{
+public:
+    Derived1(){
+        cout << "Derived constructor called\n";
+    }
+    ~Derived1(){
+        cout << "Derived destructor called\n";
+    }
+};
+
+int main()
+{
+    Base *b = new Derived1();
+    delete b;
+}
