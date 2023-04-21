d6ec8a91ec529813d36374d01c49e12202d30f56
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-04-21 18:58:07 +0530

    [ADDED]Bhavesh:The Program to illustrates the use of Constructors in multiple inheritance.

---
+++ Inheritance16.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+using namespace std;
+class A  
+{
+        private:
+                int x ;
+        public:
+                A( )    
+                {
+                          x = 0 ;
+                          cout << "\n Constructor of class A without any argument is invoked" ;
+                }
+                A(int X)  
+                {
+                          x = X ;
+                         cout << "\n Constructor of class A with one argument is invoked" ;
+                }
+                void Enter_x(void)
+                { cout << "\n\n\t Enter the value of x: " ; cin >> x ; }
+
+                void Display_x(void)
+                { cout << "\n\t x = " << x ;  }
+};
+class B   
+{
+        private:
+                int y ;
+        public:
+                B( )
+                {
+                     y = 0 ; 
+                     cout << "\n Constructor of class B without any argument is invoked" ;
+                }
+                B(int Y) 
+                {
+                        y = Y ;
+                        cout << "\n Consrtuctor of class B with one argument isinvoked" ;
+                }
+                void Enter_y(void)
+                { cout << "\t Enter the value of y: " ; cin >> y ; }
+                void Display_y(void)
+                { cout << "\n\t y = " << y ;  }
+};
+class C : public B, public A  
+{
+        private:
+                int z ;
+        public:
+                C( ) : A( ), B( ) 
+                {
+                        z = 0 ;
+                        cout << "\n Constructor of class C without any argument is invoked" ;
+                }
+                
+                C(int X,  
+                            int Y,   
+                                int Z)   
+                                        : A(X), B(Y)  
+                {
+                        z = Z ;
+                        cout << "\n Consrtuctor of class C with three arguments is invoked\n" ;
+                }
+                void Enter_z(void)
+                { cout << "\t Enter the value of z: " ; cin >> z ;  }
+                void Display_z(void)
+                { cout << "\n\t z = " << z ; }
+};
+int main()
+{
+                cout << "\n The first object c1 is in use********\n" ;
+                C c1 ;
+                c1.Enter_x( );
+                c1.Enter_y( );
+                c1.Enter_z( );
+                c1.Display_x( );
+                c1.Display_y( );
+                c1.Display_z( );
+                cout << "\n\n The second object c2 is in use********\n" ;
+                C c2(5, 6, 7) ;
+                c2.Display_x( );
+                c2.Display_y( );
+                c2.Display_z( );
+return 0;
+}
