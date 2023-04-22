a295f82898d8a4eb47702f29b6093ea151b8e84a
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-04-22 17:09:04 +0530

    [ADDED]Bhavesh:The Program to demonstrate example of single/simple inheritance.

---
+++ Inheritance20.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class A {
+public:
+    void Afun(void);
+};
+void A::Afun(void)
+{
+    cout << "I'm the body of Afun()..." << endl;
+}
+class B : public A {
+public:
+    void Bfun(void);
+};
+void B::Bfun(void)
+{
+    cout << "I'm the body of Bfun()..." << endl;
+}
+
+int main()
+{
+    B objB;
+    objB.Afun();
+    objB.Bfun();
+    return 0;
+}
