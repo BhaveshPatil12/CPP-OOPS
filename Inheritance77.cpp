255eadf8d73f0da780aae62a630c04ff922b1a67
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-05-11 09:08:21 +0530

    [ADDED]Bhavesh:The Program to print the rhombus star pattern of N rows using Multiple Inheritance. How to print rhombus or parallelogram star pattern with the help of for loop in using Multiple Inheritance.

---
+++ Inheritance77.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+class A
+{
+	protected:
+		int i,j,k;
+};
+class B
+{
+	protected:
+		int n;
+};
+class C :public A, public B
+{
+	public:
+		int T4Tutorials_Print()
+		{
+			cout<<"num of rows:";
+			cin>>n;
+			for(i=1;i<=n;i++)
+			{
+				for(j=1;j<=n-i;j++)
+				{
+					cout<<" ";
+				}
+				for(k=1;k<=n;k++)
+				{
+					cout<<"*";
+				}
+				cout<<endl;					
+					}
+		}
+};
+int main()
+{
+	C obj;
+	obj.T4Tutorials_Print();	
+}
