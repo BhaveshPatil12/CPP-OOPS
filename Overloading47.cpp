910111d8b66fd714d3408d0ddbd20c905b2077ce
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-06-22 10:12:46 +0530

    [ADDED]Bhavesh:The Program for Assignment Operator overloading.

---
+++ Overloading47.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Complex {
+private:
+	int real, img; 
+public:
+	Complex(int r, int i)
+	{
+		real = r;
+		img = i;
+	}
+	void operator=(const Complex& C)
+	{
+		real = C.real;
+		img = C.img;
+	}
+	void print() { cout << real << "+i" << img << endl; }
+};
+
+int main()
+{
+	Complex C1(2, 3), C2(4, 6);
+	cout << "BEFORE OVERLOADING ASSIGNMENT OPERATOR"<< endl;
+	cout << "C1 complex number: ";
+	C1.print();	
+	cout << "C2 complex number: ";
+	C2.print();	
+	C1 = C2;
+	cout << "AFTER OVERLOADING ASSIGNMENT OPERATOR" << endl;
+	cout << "C1 complex number: ";
+	C1.print();	
+	cout << "C2 complex number: ";
+	C2.print();
+	return 0;
+}
