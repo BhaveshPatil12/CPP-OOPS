36735daf03ba4b943081eab6f2f6a7059320e926
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-06-24 12:56:42 +0530

    [ADDED]Bhavesh:The Program to overload self-assignment operator.

---
+++ Overloading52.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<algorithm>
+class MyString
+{
+private:
+	char* m_data {};
+	int m_length {};
+
+public:
+	MyString(const char* data = nullptr, int length = 0 )
+		: m_length { std::max(length, 0) }
+	{
+		if (length)
+		{
+			m_data = new char[static_cast<std::size_t>(length)];
+
+			for (int i { 0 }; i < length; ++i)
+				m_data[i] = data[i];
+		}
+	}
+	~MyString()
+	{
+		delete[] m_data;
+	}
+	MyString& operator= (const MyString& str);
+
+	friend std::ostream& operator<<(std::ostream& out, const MyString& s);
+};
+
+std::ostream& operator<<(std::ostream& out, const MyString& s)
+{
+	out << s.m_data;
+	return out;
+}
+MyString& MyString::operator= (const MyString& str)
+{
+	if (m_data) delete[] m_data;
+	m_length = str.m_length;
+	m_data = nullptr;
+	if (m_length)
+		m_data = new char[static_cast<std::size_t>(str.m_length)];
+	for (int i { 0 }; i < str.m_length; ++i)
+		m_data[i] = str.m_data[i];
+	return *this;
+}
+
+int main()
+{
+	MyString alex("Alex", 5); 
+	MyString employee;
+	employee = alex; 
+	std::cout << employee; 
+	return 0;
+}
