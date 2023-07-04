3f3e2932389a71d25e0732010f47973b0e2defd0
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-07-04 22:31:37 +0530

    [ADDED]Bhavesh:The Program to define an Abstract Class in Polymorphism.

---
+++ Polymorphism6.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class Card
+{
+        protected:
+                char recipient[20];
+        public:
+                virtual void greeting()=0; 
+} ;
+class Holiday : public Card
+{
+        public:
+                Holiday( char r[ ] )
+                {
+                        strcpy(recipient,  r) ;
+                }
+                void greeting() 
+                {
+                        cout <<  "Dear "  <<  recipient  << endl  ;                           cout <<  "Season's Greetings!\n\n" ;
+                }
+} ;
+
+class Birthday : public Card
+{
+        private:
+                int age;
+        public:
+                Birthday ( char r[ ], int years )
+                {
+                        strcpy(recipient, r);
+                        age = years;
+                }
+                void greeting()  
+                {
+                        cout << "Dear " << recipient << ",\n" ;
+                        cout << "Happy "  << age  << "th" << " Birthday\n\n";
+                }
+} ;
+
+class Holi : public Card
+{
+        private:
+                int colors;
+        public:
+                Holi( char r[ ], int c )
+                {
+                        strcpy(recipient, r) ;
+                        colors    = c;
+                 }
+                void greeting() 
+                {
+                        cout << "Dear "  << recipient << ",\n";
+                        cout << " Happy holi and lots of colors for you\n";
+                        for ( int j=0; j < colors; j++ )
+                                cout << "*";
+                        cout << "\n\n" ;
+                }
+} ;
+
+int main ()
+{
+                Holiday   hol ( "Codez" );
+                hol.greeting();
+                Birthday  bd( "CodezClub", 21 );
+                bd.greeting();
+                Holi holi( "Max", 7 );
+                holi.greeting();
+return 0;
+}
