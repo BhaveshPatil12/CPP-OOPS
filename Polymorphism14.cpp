e78c7bde26495d0f7cafa801fffe2ebb764f80a1
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-07-07 10:13:57 +0530

    [ADDED]Bhavesh:The Program of Run Time Polymorphism.

---
+++ Polymorphism14.cpp
@@ -0,0 +1,18 @@
+#include<iostream>  
+using namespace std;
+class Base
+{
+public:
+  virtual void show()
+  {
+    cout << "Showing Base Class" << endl;
+  }
+}; 
+class Derived:public Base
+{
+public: 
+  void show()
+  {
+    cout << "Showing Derived Class" << endl; }}; int main() { Base* base_object; // pointer reference to base class Base *base_object; // creating derived class object Derived derived_object; base_object->show(); // mapping base object to address of derived class object base_object = &derived_object; // since we use pointers so we use -> rather than . 
+  return 0;
+}
