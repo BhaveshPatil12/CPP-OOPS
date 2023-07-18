c2a58e28537ddfecbe8a8a3c03c3dd15352c066e
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-07-18 10:29:52 +0530

    [ADDED]Bhavesh:The Program to Display Contents of text file.

---
+++ FileH8.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string.h>
+#include<fstream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+        ifstream ifile;
+        char s[100], fname[100];
+
+        cout<<"Enter file name to read :: ";
+        cin>>fname;
+
+        ifile.open(fname);
+
+        if(!ifile)
+        {
+                cout<<"\nError in opening file..!!\n";
+
+                exit(0);
+        }
+
+        cout<<"\n";
+
+        while(ifile.eof()==0)
+    {
+        ifile>>s;
+        cout<<s<<" ";
+    }
+        cout<<"\n";
+        ifile.close();
+return 0;
+}
