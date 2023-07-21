c499dbf289cb2657039d9d0795011d83e4a1b1cb
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-07-21 18:00:38 +0530

    [ADDED]Bhavesh:The Program to Count Words Lines and Total Size using File Handling.

---
+++ FileH15.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+     cout<<"\nText File 1 is opened........\n";
+     ifstream fin("C:\\Users\\acer\\Documents\\file4.txt"); 
+     int line=1,word=1,size;
+     char ch;
+     fin.seekg(0,ios::end); 
+     size=fin.tellg(); 
+     fin.seekg(0,ios::beg);
+     while(fin)
+     {
+      fin.get(ch);
+      if(ch==' '||ch=='\n')
+       word++;
+      if(ch=='\n')
+       line++;
+     }
+     cout<<"\nNo. of Lines = "<<line<<"\n\nNo. of Words = "<<word;
+     cout<<"\n\nSize of Text File="<<size<<"\n";
+     fin.close(); 
+     return 0;
+}
