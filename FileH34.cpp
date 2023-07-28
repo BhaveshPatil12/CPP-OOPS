c12954649e9020c22fdc38e39b285f33ee4aad15
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-07-28 10:01:03 +0530

    [ADDED]Bhavesh:The Simple Program for Read File Operation.

---
+++ FileH34.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+#include<fstream>
+int main() {
+    char c, fname[10];
+    cout << "Enter file name:";
+    cin>>fname;
+    ifstream in(fname);
+    if (!in) {
+        cout << "File Does not Exist";
+    }
+    cout << "\n\n";
+    while (in.eof() == 0) {
+        in.get(c);
+        cout << c;
+    }
+    return 0;
+}
