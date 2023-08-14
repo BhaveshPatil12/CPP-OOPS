65bb1a70c879b609ceb8ac00e954c4b17f41c625
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-08-14 10:46:56 +0530

    [ADDED]Bhavesh:The Program to demonstrate the addition of multiple types of data using generic function or template.

---
+++ TemplateT32.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+template <class Test> Test addition(Test num1, Test num2)  
+{
+        return (num1+num2);
+}
+int main()
+{
+        cout<<"\n Addition of Multiple Types of Data using Generic Template";
+        cout<<"\n Addition of Integer Parameters : "<<addition(10, 10);
+        cout<<"\n Addition of Float Parameters : "<<addition(15.5, 20.10);    
+        return 0;
+}
