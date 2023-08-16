12788440b9e61ce4e2160daecf1e97f6e8663892
BhaveshPatil12 <patilbhavesh9923@gmail.com>
2023-08-16 10:10:39 +0530

    [ADDED]Bhavesh:The Program of template for increments a value.

---
+++ TemplateT37.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+template <class T>
+class myIncrement {
+  T value;
+  public:
+  myIncrement (T arg) {value=arg;}
+  T toIncrement () {return ++value;}
+};
+template <>
+class myIncrement <char> {
+   char value;
+  public:
+   myIncrement (char arg) {value=arg;}
+   char uppercase ()
+  {
+    if ((value>='a')&&(value<='z'))
+    value+='A'-'a';
+    return value;
+  }
+};
+int main () {
+  myIncrement<int> myint (7);
+  myIncrement<char> mychar ('s');
+  myIncrement<double> mydouble(11.0);
+  
+  cout<<"Incremented int value: "<< myint.toIncrement()<< endl;
+  cout<<"Uppercase value: "<<mychar.uppercase()<< endl;
+  cout<<"Incremented double value: "<<mydouble.toIncrement()<< endl;
+  return 0;
+}
